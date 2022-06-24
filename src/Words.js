371a02afc8e2b9357dd3a40523325bd67f9d70c0
Surbhi Lande <81092552+surbhi322@users.noreply.github.com>
2022-06-24 18:15:26 +0530

    Update Words.js

--- src/Words.js
+++ src/Words.js
@@ -13,7 +13,7 @@ export const generateWordSet = async () =>{
   let wordSet;
   let todaysWord;
   await fetch(wordBank).then((response) => response.text()).then((result) => {
-        const wordArr = result.split("\r\n");
+        const wordArr = result.split("\n");
         todaysWord = wordArr[Math.floor(Math.random() *  wordArr.length)]
         wordSet = new Set(wordArr);
     });
